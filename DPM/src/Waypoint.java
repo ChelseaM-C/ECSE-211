7ffa92aab74d3c9ff5487d67bfa5654afd71a6d4
Daniel B <daniel.burke@mail.mcgill.ca>
2014-11-25 00:09:04 -0500

    Javadoc'd a bunch of crazy crap

--- DPM/src/Waypoint.java
+++ DPM/src/Waypoint.java
@@ -1,17 +1,32 @@
 	//Waypoint class
+/**
+ * Waypoint, used by navigator to travel
+ * @author Daniel
+ *
+ */
 	public class Waypoint{
 		private double wx;
 		private double wy;
-		
+		/**Create new waypoint with given coordinates (cm)
+		 * 
+		 * @param x1 x coord
+		 * @param y1 y coord
+		 */
 		public Waypoint(double x1, double y1){
 			wx = x1;
 			wy = y1;
 		}
-		
+		/**
+		 * 
+		 * @return x coord
+		 */
 		public double getX(){
 			return wx;
 		}
-		
+		 /**
+		  * 
+		  * @return y coord
+		  */
 		public double getY(){
 			return wy;
 		}
