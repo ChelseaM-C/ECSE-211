3cdb28eb0131f86a3eb64002af8a7d655a1df1c3
yuwu94 <richard_wu94@hotmail.com>
2014-11-29 18:09:32 -0500

    changes to blockPickUp

--- DPM/src/blockPickUp.java
+++ DPM/src/blockPickUp.java
@@ -7,54 +7,72 @@ public class blockPickUp extends Thread {
         public static final int FORWARD_SPEED = 200;
         public static final double forwardDistance = 12.0;
         public static final double forwardThreshold = 15.0;
-        public final double leftRadius = 2.1;
-        public final double rightRadius = 2.15;
-        public final double width = 10.0;
+        public final double leftRadius = 2.19;
+        public final double rightRadius = 2.192;
+        public final double width = 9.85;
         private ClawDriver claw;
-        private UltrasonicSensor sensor;
+        private UltrasonicController usController;
         private NXTRegulatedMotor leftMotor,rightMotor;
        
         //initiating objects
-        WheelDriver wheels = new WheelDriver( leftMotor, rightMotor) ;
+        WheelDriver wheels = new WheelDriver(leftMotor, rightMotor) ;
         Odometer odo = new Odometer();
-        Navigation nav = new Navigation(leftRadius, rightRadius, odo, wheels);
-                UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
        
-        public blockPickUp (ClawDriver claw, NXTRegulatedMotor leftMotor, NXTRegulatedMotor rightMotor ) {
+        public blockPickUp (ClawDriver claw, NXTRegulatedMotor leftMotor, NXTRegulatedMotor rightMotor, UltrasonicController usController) {
                 this.claw = claw;
                 this.leftMotor = leftMotor;
-                this.rightMotor= rightMotor;
+                this.rightMotor = rightMotor;
+                this.usController = usController;
         }
  
         public void scanRange() {
                
                 int threshold = 20;            
-                boolean travel = true;
                 double turnAngle = -10;
                 int storeI = 0;
                 int storeW = 0;
                 int loopBreak = 0;
                 int secondLap = 0;
+ 
+                boolean[] angleDirection = new boolean[9];
                
-                while (travel){                
-                        if (us.getDistance() < threshold) {
-                                travelForward(forwardThreshold);
-                                claw.close();
+                //this loop continues until the block is found
+                while (true){
+               
+                                //first check, if block in front, move forward and exit while loop
+                        if (usController.getFilteredDist() < threshold) {
+//                                travelForward(forwardThreshold);
+//                                claw.close();
                                 break;
                         }
+                        //else start scanning starting from CCW 60 degrees
                         rotateCCW(60);
-                       
+                        //check every angle in the interval of 10 degrees between 60 degrees CCW to 30 degree CW
+                        //if block is found, exit loop
+                        //if not, then enter the next if statement
                         for (int i = 0 ; i < 9 ; i++){
                                 rotateCCW(turnAngle);
-                                if (us.getDistance() < threshold){
-                                        travelForward(forwardThreshold);
-                                        claw.close();
-                                        loopBreak = 1;
-                                        storeI = i;
-                                        break;
+                                if (usController.getFilteredDist() < threshold){
+                                                angleDirection[i] = true;                                              
+                                                Sound.beep();
+                               
+                                                if (i > 0){
+ 
+                                                        if (angleDirection[i-1] && angleDirection[i]){
+                                                                Sound.twoBeeps();
+                                                                rotateCCW(-turnAngle/2);
+                                                                storeI = i;
+                                                                LCD.drawString("i:"+storeI, 2, 5);
+                                                                LCD.drawString("W: "+storeW, 2, 6);
+                                                        loopBreak = 1;
+                                                        break;
+                                                }
+                                                }      
                                 }
+                                   
                         }
-                       
+                        //if loop is not broken, turn back to forward direction from 30 degrees CW
+                        //move forward by certain distance, and continue scan again
                         if (loopBreak != 1){
                                 rotateCCW(30);
                                 travelForward(forwardDistance);
@@ -76,26 +94,29 @@ public class blockPickUp extends Thread {
                                 break;
                         }
                 }
-                   
+               
+                travelForward(forwardThreshold);
+                claw.close();
+ 
                 Sound.beep();
                 rotateCCW(-180);
                 travelForward(forwardThreshold);
                
-                if (storeI <= 5) {
+                if (loopBreak > 0 && storeI < 6){
                         Sound.buzz();
-                        rotateCCW(storeI*turnAngle);
-                }
-                if (storeI >= 7){
-                        rotateCCW(-storeI*turnAngle);
-                }
-               
+                    rotateCCW((6.5 - storeI)*turnAngle);
+                    }
+                if (loopBreak > 0 && storeI > 6){
+                        rotateCCW(-(storeI - 6.5)*turnAngle);
+                        }
+                Sound.buzz();
                 travelForward(storeW*forwardDistance);
                
-                if (secondLap > 0){
-                        rotateCCW(90);
-                        travelForward(30);
-                        rotateCCW(-90);
-                }
+//                if (secondLap > 0){
+//                        rotateCCW(90);
+//                        travelForward(30);
+//                        rotateCCW(-90);
+//                }
                
         }
                
@@ -106,7 +127,7 @@ public class blockPickUp extends Thread {
                 rightMotor.rotate(convertDistance(rightRadius, forwardDistance), false);
         }
        
-        //rotate 45 degrees counterclockwise
+        //rotate counterclockwise
         public void rotateCCW(double angle) {
             leftMotor.setSpeed(ROTATE_SPEED);
             rightMotor.setSpeed(ROTATE_SPEED);
@@ -114,18 +135,6 @@ public class blockPickUp extends Thread {
             rightMotor.rotate(convertAngle(rightRadius, width, angle), false);
            
         }
-       
-        private int getFilteredData() {
-            int distance;
-            // do a ping
-            us.ping();
-            // wait for the ping to complete
-            try { Thread.sleep(50); } catch (InterruptedException e) {}
-            // there will be a delay here
-            distance = us.getDistance();
-            return distance;
-    }
-       
        
         private static int convertDistance(double radius, double distance) {
                 return (int) ((180.0 * distance) / (Math.PI * radius));
