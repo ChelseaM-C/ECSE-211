ece0d50470edc75f01a86f23aad90228016da42c
wiizi <wei.wang18@mail.mcgill.ca>
2014-11-30 20:19:04 -0500

    Pick up both left and right sides

--- DPM/src/blockPickUp.java
+++ DPM/src/blockPickUp.java
@@ -1,7 +1,5 @@
 import lejos.nxt.*;
- 
- 
- 
+
 public class blockPickUp extends Thread {
     public static final int ROTATE_SPEED = 150;
     public static final int FORWARD_SPEED = 200;
@@ -27,17 +25,21 @@ public class blockPickUp extends Thread {
         int storeW = 0;
         int loopBreak = 0;
         int secondLap = 0;
+        int leftRight = -1;
 
-        boolean[] angleDirection = new boolean[12];
+        
+        boolean[] turnDirection = new boolean [14];
 
         //this loop continues until the block is found
         while (true){
+            
+            boolean[] blockDetect = new boolean[12];
         //first check, if block in front, move forward and exit while loop
             if (usController.getFilteredDist() < threshold) {
                 break;
             }
-            //else start scanning starting from CCW 60 degrees
-            rotateCCW(60);
+            //else start scanning starting from CCW 60
+            rotateCCW(65);
             //check every angle in the interval of 10 degrees between 60 degrees CCW to 60 degree CW
             //if block is found, exit loop
             //if not, then enter the next if statement
@@ -45,13 +47,11 @@ public class blockPickUp extends Thread {
                 rotateCCW(turnAngle);
 
                 if (usController.getFilteredDist() < threshold){
-                    angleDirection[i] = true;
-                    Sound.beep();
+                    blockDetect[i] = true;
 
                     if (i > 0){
-                        if (angleDirection[i-1] && angleDirection[i]){
-                            Sound.twoBeeps();
-                            rotateCCW(-turnAngle/2);
+                        if (blockDetect[i-1] && blockDetect[i]){
+                            rotateCCW(- 0.5 * turnAngle);
                             storeI = i;
                             LCD.drawString("i:"+storeI, 2, 5);
                             LCD.drawString("W: "+storeW, 2, 6);
@@ -68,42 +68,70 @@ public class blockPickUp extends Thread {
                 travelForward(forwardDistance);
                 storeW++;
 
-                if (storeW >= 5){
-                    rotateCCW(180);
-                    travelForward(storeW*forwardDistance);
-                    rotateCCW(-90);
-                    travelForward(30);
-                    rotateCCW(-90);
+                if (storeW > 1){
+                    rotateCCW(110);
+                    for (int i = 0 ; i < 15; i++){
+                        rotateCCW(-turnAngle);
+                        if (usController.getFilteredDist() < threshold + 10){
+                            Sound.beep();
+                            turnDirection[i] = true;
+                            leftRight = i;
+                            break;
+                        }
+                    }
+                    if (leftRight == -1){
+                        rotateCCW(-70);
+                        travelForward(storeW*forwardDistance);
+                        rotateCCW(-180);
+                    }
+                    if (leftRight > -1 && leftRight <= 7){
+                        rotateCCW((leftRight - 7)* turnAngle);
+                        travelForward(storeW*forwardDistance);
+                        rotateCCW(-90);
+                        travelForward(30);
+                        rotateCCW(-90);
+                        secondLap++;
+                    } else if (leftRight > 7){
+                        rotateCCW((leftRight - 7)*turnAngle);
+                        travelForward(storeW*forwardDistance);
+                        rotateCCW(90);
+                        travelForward(30);
+                        rotateCCW(90);
+                        secondLap++;
+                    }
                     storeI = 0;
                     storeW = 0;
-                    secondLap++;
                     scanRange();
                 }
             } else{
                 break;
             }
         }
-
         travelForward(forwardThreshold);
         claw.close();
-
-        Sound.beep();
-        rotateCCW(-180);
-        travelForward(forwardThreshold);
+        Sound.twoBeeps();
 
         if (loopBreak > 0 && storeI < 6){
-            Sound.buzz();
+            rotateCCW(180);
+            travelForward(forwardThreshold);
             rotateCCW((6.5 - storeI)*turnAngle);
+            travelForward(storeW*forwardDistance);
         } else if (loopBreak > 0 && storeI > 6){
-            rotateCCW(-(storeI - 6.5)*turnAngle);
+            rotateCCW(180);
+            travelForward(forwardThreshold);
+            rotateCCW((6.5 - storeI)*turnAngle);
+            travelForward(storeW*forwardDistance);
         }
-        Sound.buzz();
-        travelForward(storeW*forwardDistance);
-
-        if (secondLap > 0){
+        
+        if (secondLap > 0 && leftRight <= 6){
             rotateCCW(90);
             travelForward(30);
             rotateCCW(-90);
+        }
+        if (secondLap > 0 && leftRight > 6){
+            rotateCCW(-90);
+            travelForward(30);
+            rotateCCW(90);
         }
     }
 
